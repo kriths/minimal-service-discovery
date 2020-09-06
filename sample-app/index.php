0c419abfbccfb2f8b919a50afce89fbd434e2a10
Thomas Krisch <thomas.krisch+git@gmail.com>
2020-09-06 23:53:59 +0200

    Kill switch for servers

--- sample-app/index.php
+++ sample-app/index.php
@@ -2,6 +2,14 @@
 <body>
 <h1>Sample App</h1>
 Instance-ID: <?=file_get_contents("http://169.254.169.254/latest/meta-data/instance-id") ?><br>
-Public IP: <?=file_get_contents("http://169.254.169.254/latest/meta-data/public-ipv4") ?>
+Public IP: <?=file_get_contents("http://169.254.169.254/latest/meta-data/public-ipv4") ?><br>
+<a href="?kill">Kill</a>
 </body>
 </html>
+
+<?php
+if (isset($_REQUEST['kill'])) {
+    // Remove self if kill query / post variable is set
+    unlink(__FILE__);
+}
+?>
